2aeda324fe52cde92f08c447b5180c83853a97f8
feerbax56 <94625489+feerbax56@users.noreply.github.com>
2022-08-02 21:24:08 +0500

    add: stars modules

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,46 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return (
+        <div>
+            This is APP component
+            <Rating/>
+            <Accordion/>
+        </div>
+    );
+}
+
+function Rating() {
+    return (
+        <>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </>
+    )
+}
+
+function Star() {
+    return (
+        <div> StarStarStar</div>
+    )
+
+}
+
+function Accordion() {
+    return <div>
+        <h1>HOHOHO</h1>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
     </div>
-  );
 }
 
 export default App;
