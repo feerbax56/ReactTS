e8baca319294ed658e7107fe5e05c328ce74b206
feerbax56 <94625489+feerbax56@users.noreply.github.com>
2022-08-02 21:58:01 +0500

    add: props fot stars

--- src/components/rating/Rating.tsx
+++ src/components/rating/Rating.tsx
@@ -2,22 +2,25 @@ import React from "react";
 
 export function Rating() {
     return (
-        <>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-        </>
+        <div>
+            <Star selected={true}/>
+            <Star selected={true}/>
+            <Star selected={true}/>
+            <Star selected={true}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+        </div>
     )
 }
 
-function Star() {
-    return (
-        <div> StarStarStar</div>
-    )
+function Star(props: any) {
+    if (props.selected === true) {
+        return (<span><b>*Star*</b>  </span>)
+    } else {
+        return (<span>*Star* </span>)
+    }
+
 
 }
